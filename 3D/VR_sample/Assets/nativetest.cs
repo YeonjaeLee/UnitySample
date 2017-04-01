5f1e94cba187e1d367b4fba86d50a068096eddb1
YeonjaeLee(living) <yeonjae115@gmail.com>
2017-04-01 13:39:29 +0900

    Unity 3D: VR_sample 뷰포리아 연결 but 이미지 출력 오류

---
+++ 3D/VR_sample/Assets/nativetest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nativetest : MonoBehaviour {
+    public void CallFromNative(string _strLog)
+    {
+        Debug.Log(_strLog);
+    }
+    //// Use this for initialization
+    //void Start () {
+	
+    //}
+	
+    //// Update is called once per frame
+    //void Update () {
+	
+    //}
+}
